1c2b84c6fb555d1bb55ca95a0c2046657e5b7f66
Alisher <mamatkulovalisher20@gmail.com>
2023-07-27 16:54:55 +0500

    js 1-dars
    22-guruh oquvchisi

---
+++ js/main.js
@@ -0,0 +1,55 @@
+/*console.log(5*5)*/
+/*alert("Saytimizga xushkelibsiz!")*/
+/*document.write("Men body oynasida korinaman!")*/
+
+/*let x = 5;*/
+/*let y = 5;*/
+/*x = 10*/
+
+/*let x = 2, y = 5, z = 8;*/
+/*document.write(x+y+z)*/
+
+/*let salom = "Assalomu aleykum!"*/
+/*let rang = "#f1f1f1"*/
+/*document.write(rang)*/
+
+/* Data types in JS*/
+/*let x = 5;*/   /* number */
+/*let matn = "JavaScript"*/  /* Sting */
+/*document.write(typeof matn);*/
+
+/*Math in JS*/
+
+/*let x = 55;*/
+/*let matn = "20"*/
+/*x+=1;*/ /*5+1*/
+/*x-1*/ /* x=5-1 */
+/*x*=1;*/ /* x=5*1 */
+/*x/=1*/ /* x=5/1 */
+/*let qoldiq = x%6*/
+/*let daraja = x**2*/  /* 5*5 */
+/*++x;*/
+/*--x;*/
+/*x++;*/
+/*x--;*/
+/*document.write(x+matn)*/
+
+/* if (agar) */
+/* else (u holda) */
+/* else if (yoki bo'lmasa) */
+
+/*if ("salom" == "salom"){
+    console.log("Ular teng");
+}*/
+/*else {
+    console.log("Ular teng emas")
+}*/
+if (45 == 44){
+    console.log("Birinchi qiymatlar teng!")
+}
+else if (45 == 46){
+    console.log("ikkinchisidagi qiymatlar teng!")
+}
+else {
+    console.log("ikkalasidagi qiymatlar teng emas!")
+}
